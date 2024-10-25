702c92182fed161c0b68bba9715fb43302316a54
Swapnil Gumber <114899065+SwapnilGumber@users.noreply.github.com>
2024-10-25 10:49:25 +0000

    random puzzle generator

--- sudoku.js
+++ sudoku.js
@@ -3,99 +3,162 @@ var tileSelected = null;
 var timer;
 var seconds = 0;
 var errors = 0;
-
-var board = [ 
-    "--74916-5",
-    "2---6-3-9",
-    "-----7-1-",
-    "-586----4",
-    "--3----9-",
-    "--62--187",
-    "9-4-7---2",
-    "67-83----",
-    "81--45---"
-]
-var solution = [
-    "387491625",
-    "241568379",
-    "569327418",
-    "758619234",
-    "123784596",
-    "496253187",
-    "934176852",
-    "675832941",
-    "812945763"
-]
+var board = [];
+var solution = [];
 
 window.onload = function() {
+    generateRandomBoard();
     setGame();
     startTimer();
 }
 
-function setGame(){
+function generateRandomBoard() {
+    // Generate a complete valid Sudoku board
+    solution = generateCompleteBoard();
+    // Create a puzzle with some numbers removed
+    board = removeNumbers(solution);
+}
+
+function generateCompleteBoard() {
+    // Fill the board using a backtracking algorithm
+    let newBoard = Array.from({ length: 9 }, () => Array(9).fill(0));
+
+    function fillBoard(row, col) {
+        if (row === 9) return true; // All rows are filled
+
+        let nextRow = (col === 8) ? row + 1 : row;
+        let nextCol = (col + 1) % 9;
+
+        let numbers = shuffleArray([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+        for (let number of numbers) {
+            if (isValidPlacement(newBoard, row, col, number)) {
+                newBoard[row][col] = number;
+                if (fillBoard(nextRow, nextCol)) return true;
+                newBoard[row][col] = 0; // Backtrack
+            }
+        }
+        return false;
+    }
+
+    fillBoard(0, 0);
+    return newBoard.map(row => row.join(""));
+}
+
+function removeNumbers(board) {
+    let newBoard = board.map(row => row.split(""));
+    let clues = 30; // Number of cells to leave filled
+
+    for (let i = 0; i < 81 - clues; i++) {
+        let row, col;
+        do {
+            row = Math.floor(Math.random() * 9);
+            col = Math.floor(Math.random() * 9);
+        } while (newBoard[row][col] === "-");
+
+        newBoard[row][col] = "-";
+    }
+    return newBoard.map(row => row.join(""));
+}
+
+function isValidPlacement(board, row, col, number) {
+    // Check the row
+    if (board[row].includes(number)) return false;
+    // Check the column
+    for (let r = 0; r < 9; r++) {
+        if (board[r][col] === number) return false;
+    }
+    // Check the 3x3 sub-grid
+    let boxRow = Math.floor(row / 3) * 3;
+    let boxCol = Math.floor(col / 3) * 3;
+    for (let r = boxRow; r < boxRow + 3; r++) {
+        for (let c = boxCol; c < boxCol + 3; c++) {
+            if (board[r][c] === number) return false;
+        }
+    }
+    return true;
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+function setGame() {
     // digits 1-9
-    for(let i=1; i<=9; i++){
-        // <div id="i" class="number">i</div>
+    for(let i = 1; i <= 9; i++) {
         let number = document.createElement("div");
         number.id = i;
         number.innerText = i;
-        number.addEventListener("click",selectNumber);
+        number.addEventListener("click", selectNumber);
         number.classList.add("number");
         document.getElementById("digits").appendChild(number);
     }
     // board 9*9
-    for(let r=0; r<9; r++){
-        for(let c=0; c<9; c++){
+    for(let r = 0; r < 9; r++) {
+        for(let c = 0; c < 9; c++) {
             let tile = document.createElement("div");
             tile.id = r.toString() + '-' + c.toString();
-            if(board[r][c]!='-'){
+            if(board[r][c] !== '-') {
                 tile.innerText = board[r][c];
                 tile.classList.add("tile-start");
             }
-            if(r==2 || r==5){
+            if(r === 2 || r === 5) {
                 tile.classList.add("horizontal-line");
             }
-            if(c==2 || c==5){
+            if(c === 2 || c === 5) {
                 tile.classList.add("vertical-line");
             }
-            tile.addEventListener("click",selectTile)
+            tile.addEventListener("click", selectTile);
             tile.classList.add("tile");
             document.getElementById("board").append(tile);
         }
     }
 }
 
-function selectNumber(){
-    if(numSelected != null){
+function selectNumber() {
+    if(numSelected != null) {
         numSelected.classList.remove("number-selected");
     }
     numSelected = this;
-    numSelected.classList.add("number-selected"); 
+    numSelected.classList.add("number-selected");
 }
 
-function selectTile(){
-    if(numSelected){
-        if(this.innerText != ""){
+function selectTile() {
+    if(numSelected) {
+        if(this.innerText != "") {
             return;
         }
 
-        // "0-0" "0-1" ... "3-1"
-        let coords = this.id.split("-"); //["0","0"]
+        let coords = this.id.split("-");
         let r = parseInt(coords[0]);
         let c = parseInt(coords[1]);
 
-        if(solution[r][c] == numSelected.id){
+        if(isValidPlacement(board, r, c, parseInt(numSelected.id))) {
             this.innerText = numSelected.id;
+            board[r][c] = numSelected.id;
             if (checkBoardSolved()) {
                 alert("Congratulations! You solved the Sudoku puzzle!");
                 reloadPage();
             }
-        }
-        else{
+        } else {
             errors += 1;
             document.getElementById("errors").innerText = errors;
         }
     }
+}
+
+function checkBoardSolved() {
+    for (let r = 0; r < 9; r++) {
+        for (let c = 0; c < 9; c++) {
+            if (board[r][c] !== solution[r][c]) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
 
 function startTimer() {
@@ -113,19 +176,8 @@ function formatTime(seconds) {
     return minutes + ":" + secs;
 }
 
-function checkBoardSolved() {
-    for (let r = 0; r < 9; r++) {
-        for (let c = 0; c < 9; c++) {
-            let tile = document.getElementById(r.toString() + '-' + c.toString());
-            if (tile.innerText != solution[r][c]) {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-function reloadPage(){
+function reloadPage() {
     setTimeout(function() {
         location.reload();
-    }, 1000); 
+    }, 1000);
 }
